0f87ec69d004ae1f9b5102f00823607e845daa61
Brian Jan Carlos <41847348+brianjancarlos@users.noreply.github.com>
2026-01-17 19:00:58 +0800

    Update MongoDB connection URI for testing

--- mongodbtest.js
+++ mongodbtest.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://japanssw-rw:Jz1SyuD8peLkwMzl@japansswcluster0.lvia1ct.mongodb.net/?appName=japansswCluster0";
+const uri = "mongodb+srv://u:p@japansswcluster0.lvia1ct.mongodb.net/?appName=japansswCluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
